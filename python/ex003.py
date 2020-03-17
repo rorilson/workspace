ba20d96aebfc5bdbb5eb5af16c2633664d27113a
rorilson <rorilson@gmail.com>
2020-03-16 23:36:20 -0300

    int

--- python/ex003.py
+++ python/ex003.py
@@ -1,4 +1,4 @@
-n1 = input('Digite um valor: ')
-n2 = input('Digite outro valor: ')
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
 s = n1 + n2
-print('A soma entre {} e {} é igual a {}!'.format(n1,n2,s))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
